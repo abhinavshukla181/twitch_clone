e0101a01ed6ec07e07166fb19207224b61fa1366
Abhinav shukla <abhinavshukla181@gmail.com>
2022-11-04 11:58:44 +0530

    twitch 4 done here

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:twitch_clone/models/livestream.dart';
-import 'package:twitch_clone/models/users.dart';
 import 'package:twitch_clone/providers/user_provider.dart';
 import 'package:twitch_clone/resources/storage_methods.dart';
 import 'package:twitch_clone/utils/utils.dart';
@@ -17,36 +16,37 @@ class FireStoreMethods {
       BuildContext context, String title, Uint8List? image) async {
     final user = Provider.of<UserProvider>(context, listen: false);
     String channelId = '';
+    channelId = '${user.user.uid}${user.user.username}';
     try {
       if (title.isNotEmpty && image != null) {
-        if (!(await _firestore
-                .collection('livestream')
-                .doc(user.user.uid)
-                .get())
-            .exists) {
-          String thumbnailUrl = await _storageMethods.uploadImageToStorage(
-            'livestream-thumbnails',
-            image,
-            user.user.uid,
-          );
+        if (channelId != null && channelId.isEmpty) {
+          print('Ykkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
+          var docUser = _firestore.collection('livestream').doc();
+          // .doc('${user.user.uid}${user.user.username}');
+          if (!((await docUser.get()).exists)) {
+            String thumbnailUrl = await _storageMethods.uploadImageToStorage(
+              'livestream-thumbnails',
+              image,
+              user.user.uid,
+            );
 
-          //useridusername is channelid
-          channelId = '${user.user.uid}${user.user.username}';
-          LiveStream liveStream = LiveStream(
+            //useridusername is channelid
+
+            LiveStream liveStream = LiveStream(
               title: title,
               image: thumbnailUrl,
               uid: user.user.uid,
               username: user.user.username,
               viewers: 0,
               channelId: channelId,
-              startedAt: DateTime.now());
+              startedAt: DateTime.now(),
+            );
 
-          _firestore
-              .collection('livestream')
-              .doc(channelId)
-              .set(liveStream.toMap());
-        } else {
-          showSnackBar(context, 'Two livestream are not possible at same time');
+            docUser.set(liveStream.toMap());
+          } else {
+            showSnackBar(
+                context, 'Two livestream are not possible at same time');
+          }
         }
       } else {
         showSnackBar(context, 'Please enter all the fields');
